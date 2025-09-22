379329b29eda25a71d0fc9ff3120d171f97f46d7
Rajat1120 <rajat812678@gmail.com>
2025-09-22 16:24:00 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="h-20 bg-[#f2f7f3] px-10 flex items-end gap-x-10 justify-start">
+      <button className="p-4 bg-white">Agriculture</button>
+      <button className="p-4 bg-white">Products</button>
+      <button className="p-4 bg-white">About</button>
+      <button className="p-4 bg-white">Contact Us</button>
+    </div>
+  );
+};
+
+export default Header;
